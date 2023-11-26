21f0c63f0b9118a1f1304835894c7823ff5bfad5
SSameTT <sametemsen@gmail.com>
2023-11-26 14:06:44 +0300

    perşembe eklendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("bugün pazartesi");
-        System.out.println("yarın salı");
+        System.out.println("yarın salı ");
+        System.out.println("burcu Perşembe ");
+
     }
 
 }
